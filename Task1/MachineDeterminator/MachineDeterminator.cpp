1e86b25dbbe95acd8382988a2fee1731c9a3cbfa
NiCrOmAnT <61577550+NiCrOmAnT@users.noreply.github.com>
2021-12-01 22:18:41 +0300

    Сделал детерминатор

--- Task1/MachineDeterminator/MachineDeterminator.cpp
+++ Task1/MachineDeterminator/MachineDeterminator.cpp
@@ -2,13 +2,19 @@
 #include <string>
 #include <sstream>
 #include <map>
+#include <set>
 
 using namespace std;
 
 struct Options {
     bool isLeftSided = false;
     int lineAmount = 0;
-    map<char, map<char, char>> transitionMatrix;
+    map<string, map<string, string>> transitionMatrix;
+    map<string, map<string, string>> resultMatrix;
+    set<string> symbols;
+    set<string> states;
+    string startState;
+    string endState;
 };
 
 Options parseInput(istream& in)
@@ -24,29 +30,92 @@ Options parseInput(istream& in)
         string inputState;
         string outputState;
         getline(in, inputState, ' ');
+        if (i == 0)
+            options.startState = inputState;
+        if (options.isLeftSided)
+        {
+            options.endState = "S";
+            options.startState = "H";
+        }
+        else
+        {
+            options.endState = "H";
+            options.startState = "S";
+        }
         getline(in, outputState);
         istringstream iss(outputState);
-        map<char, char> transitions;
         string transition;
         while (getline(iss, transition, '|'))
         {
             if (options.isLeftSided)
-                transitions[transition[0]] = transition[1];
+            {
+                string state{ transition.size() >= 2 ? transition[0] : 'H' };
+                string symbol{ transition.size() >= 2 ? transition[1] : transition[0]};
+                options.symbols.insert(symbol);
+                if (options.transitionMatrix[state][symbol].find(inputState) == string::npos)
+                    options.transitionMatrix[state][symbol] += inputState;
+            }
             else
-                transitions[transition[1]] = transition[0];
+            {
+                string state = { transition.size() >= 2 ? transition[1] : 'H' };
+                string symbol{ transition[0] };
+                options.symbols.insert(symbol);
+                if (options.transitionMatrix[inputState][symbol].find(state) == string::npos)
+                    options.transitionMatrix[inputState][symbol] += state;
+            }
         }
-        if (options.isLeftSided)
-            options.transitionMatrix[inputState[0]] = transitions;
+    }
+    return options;
+}
+
+void determinateMatrix(Options& options)
+{
+    options.states.insert(options.startState);
+    options.resultMatrix[options.startState] = options.transitionMatrix[options.startState];
+    for (auto& inputState : options.states)
+    {
+        for (auto& pair : options.transitionMatrix[inputState])
+        {
+            options.states.insert(pair.second);
+            if (pair.second.size() == 1 || options.transitionMatrix.count(pair.second) == 1)
+            {
+                options.resultMatrix[pair.second] = options.transitionMatrix[pair.second];
+                continue;
+            }
+            map<string, string> transitions;
+            for (char charState : pair.second)
+            {
+                for (auto i = options.symbols.begin(); i != options.symbols.end(); i++) 
+                {
+                    string symbol = *i;
+                    string state{ charState };
+                    for (auto it = options.transitionMatrix[state][symbol].begin(); it != options.transitionMatrix[state][symbol].end(); it++)
+                        if (transitions[symbol].find(*it) == string::npos)
+                            transitions[symbol] += *it;
+                }
+            }
+            options.resultMatrix[pair.second] = transitions;
+            options.transitionMatrix[pair.second] = transitions;
+        }
     }
 }
 
-map<char, map<char, char>> determinateMatrix(Options options)
+void writeResult(Options& options, ostream& out)
 {
-
+    for (auto& statePair : options.resultMatrix)
+    {
+        for (auto& pair : statePair.second)
+        {
+            if (pair.second == "")
+                continue;
+            out << statePair.first << " " << pair.first << " " << pair.second << endl;
+        }
+    }
 }
-
 
 int main()
 {
     auto options = parseInput(cin);
+    determinateMatrix(options);
+    writeResult(options, cout);
 }
